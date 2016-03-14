2d5fc1269dbdb192d387e1241020e4075538987e
Corey Light <coreylight@gmail.com>
2016-03-14 15:47:58 -0500

    fixin

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,10 +1,6 @@
 import * as admin from './admin';
 import * as analytics from './analytics';
 import * as app from './app';
-import * as checks from './checks';
-import * as env from './env';
-import * as integrations from './integrations';
-import * as onboard from './onboard';
 import * as search from './search';
 import * as user from './user';
 
@@ -12,10 +8,6 @@ export {
   admin,
   analytics,
   app,
-  checks,
-  env,
-  integrations,
-  onboard,
   search,
   user
 };
