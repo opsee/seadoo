5d3353f1190ce4c1ba338ad705f5aa677ed937b6
Corey Light <coreylight@gmail.com>
2016-03-31 10:53:29 -0500

    slimmin

--- config/default.js
+++ config/default.js
@@ -1,13 +1,5 @@
 export default {
   access_key: null,
-  checkDefaultPath: null,
-  checkDefaultPort: null,
-  checkDefaultProtocol: 'http',
-  checkDefaultVerb: 'GET',
-  defaultBastionRegion: null,
-  ghosting: false,
-  googleAnalyticsID: 'UA-59205908-3',
-  remoteDebugPort: null,
   secret_key: null,
   services: {
     analytics: 'https://myst.opsee.com',
@@ -15,9 +7,5 @@ export default {
     auth: 'https://auth.opsee.com',
     stream: 'wss://api.opsee.com/stream/',
     compost: 'https://compost.in.opsee.com'
-  },
-  //display vpc screen despite only having 1 vpc in environment
-  showVpcScreen: false,
-  //allow viewing parts of the application that need a bastion to function properly
-  skipBastionRequirement: false
+  }
 };
